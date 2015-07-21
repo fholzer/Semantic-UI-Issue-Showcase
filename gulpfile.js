a8356f00ef3ee10f0c316a920ac19e61ab1f88ca
Ferdinand Holzer <ferdinand.holzer@bwinparty.com>
2015-07-21 12:58:25 +0200

    suggested gulpfile for semantic ui users
    this also works well when not using gulp-help

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp-help')(require('gulp'));
 var console = require('better-console');
-var watch = require('./semantic/tasks/watch');
-var build = require('./semantic/tasks/build');
 
 gulp.task('default', function(done) {
 	console.log("Run run gulp build and watch tasks to showcase the issue.");
 	done();
 });
 
-gulp.task('build', build);
-gulp.task('watch', watch);
+gulp.task('init', ['build:semantic']);
+
+gulp.task('build:semantic', function(done) {
+	var build = require('./semantic/tasks/build');
+	build(done);
+
+	// instead they could even do this:
+	//require('./semantic/tasks/build')(done);
+});
+
+
+gulp.task('watch', function(done) {
+	// watch some project related files
+	// and also semantic ui files
+	var watch = require('./semantic/tasks/watch');
+	watch();
+
+	// instead they could even do this:
+	//require('./semantic/tasks/watch')(done);
+});
